e142ae958dd8964d180e457da2da7cbf2a9a2726
andrianoavy <abrahamramilison+andrianoavy@gmail.com>
2023-01-11 14:48:06 +0300

    Structure config db+server  et routes

---
+++ app/controllers/example-controller.js
@@ -0,0 +1,12 @@
+function get(req,res){
+    console.log("Example controller - GET");
+}
+
+function post(req,res){
+    console.log("Example controller - POST");
+}
+
+module.exports = {
+    get,
+    post
+}
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //point d'entrée server
+const db = require('./app/config/mongodb-config')
+const router = require('./app/router')
+    
+
+db.connect(
+    () => {
+        console.log("Connected to Database");
+        // db.get().createCollection("test", {} ).then(
+        //     (collection) => {
+        //         console.log("Collection user créé");
+        //     }
+        // )
+        router.start( () => {
+            console.log(`Server is running.`);
+        })
+    }
+)
+
+
+// db.get().createCollection('test')
